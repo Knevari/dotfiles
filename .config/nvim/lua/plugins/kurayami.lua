bcf632777661aeae7bd9a3e2d92f090228cf4fe4
Knevari <winresource000@gmail.com>
2025-05-24 00:14:52 -0300

    language support, case navigation plugin

--- .config/nvim/lua/plugins/kurayami.lua
+++ .config/nvim/lua/plugins/kurayami.lua
@@ -1,9 +1,10 @@
-return {
-  "kevinm6/kurayami.nvim",
-  event = "VimEnter", -- load plugin on VimEnter or
-  -- lazy = false,                  --   don't lazy load plugin
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("kurayami") -- this is enough to initialize and load plugin
-  end,
-}
+return {}
+-- return {
+--   "kevinm6/kurayami.nvim",
+--   event = "VimEnter", -- load plugin on VimEnter or
+--   -- lazy = false,                  --   don't lazy load plugin
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("kurayami") -- this is enough to initialize and load plugin
+--   end,
+-- }
--- .config/nvim/lua/plugins/nordic.lua
+++ .config/nvim/lua/plugins/nordic.lua
@@ -1,9 +1,8 @@
-return {}
--- return {
---  "AlexvZyl/nordic.nvim",
---  lazy = false,
--- priority = 1000,
--- config = function()
--- require("nordic").load()
--- end,
--- }
+return {
+  "AlexvZyl/nordic.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nordic").load()
+  end,
+}
